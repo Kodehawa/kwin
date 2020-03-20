a27106ea6fc250ac89b27a36f9959256dec804e1
Ismael Asensio <isma.af@gmail.com>
2020-03-20 18:01:26 +0100

    Fix copyright dates

--- kcmkwin/kwinrules/rulesdialog.h
+++ kcmkwin/kwinrules/rulesdialog.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2004 Lubos Lunak <l.lunak@kde.org>
- * Copyright (c) 2010 Ismael Asensio <isma.af@gmail.com>
+ * Copyright (c) 2020 Ismael Asensio <isma.af@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
